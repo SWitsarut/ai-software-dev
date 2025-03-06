9390a42d0861c8f0d1d50b6602270dd471f4614d
SWitsarut <witsarut1133@outlook.com>
2025-03-06 18:23:39 +0700

    setup express files server

--- node_server/src/index.ts
+++ node_server/src/index.ts
@@ -2,21 +2,41 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from "cors";
 import Stripe from "stripe";
+import path from 'path';
+// import fs from 'fs';
 
 const app = express();
 
-dotenv.config()
-app.use(cors())
-app.use(express.json())
+dotenv.config();
+app.use(cors());
+app.use(express.json());
 
 const PORT: number = 8080;
 
 const stripeKey = process.env.STRIPE_PRIVATE || "";
 const stripe = new Stripe(stripeKey);
 
+// Serve static files
+app.use("/point_clouds",express.static( "public"));
+
+// File listing for /public
+// app.get("/public", (_, res) => {
+//   const publicDir = path.join(__dirname, "public");
+
+//   fs.readdir(publicDir, (err, files) => {
+//     if (err) {
+//       return res.status(500).send(err);
+//     }
+
+//     // Generate links to files
+//     const fileLinks = files.map(file => `<a href="/public/${file}">${file}</a>`).join("<br>");
+//     res.send(`<h1>File List</h1>${fileLinks}`);
+//   });
+// });
+
+
 app.get('/', (_, res) => {
-  // res.send('Express + TypeScript Server');
-  res.json({ "msg": "SIGMA" })
+  res.send('Express + TypeScript Server');
 });
 
 app.post("/checkout", async (_, res) => {
@@ -52,6 +72,8 @@ app.post("/checkout", async (_, res) => {
   res.json(session)
 
 })
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
